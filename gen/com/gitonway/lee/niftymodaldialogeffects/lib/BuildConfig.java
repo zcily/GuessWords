ff9b89ba644292850913eb42d82e4fea2d8899bc
zcily <zhangwuyi55@hotmail.com>
2015-08-27 09:38:33 +0800

    add asserts folder and rm bin gen folder

--- gen/com/gitonway/lee/niftymodaldialogeffects/lib/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.gitonway.lee.niftymodaldialogeffects.lib;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
